2e187186187f0231dae49915499782d037dec096
Rubens Flinco <rubens@planetsweb.com.br>
2019-09-03 22:26:07 -0300

    video

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ import {
   StyleSheet,
   Text,
   View,
+  Environment, 
+  VideoModule, 
+  staticResourceURL
 } from 'react-360';
+
+import {} from 'react-360';
+
+// Create a player
+const player = VideoModule.createPlayer('myplayer');
+// Play a specific video
+player.play({
+  source: {url: staticResourceURL('video.mp4')}, // provide the path to the video
+  stereo: '3DTB', // optionally, supply the format of the video
+});
+// Display the background video on the Environment
+Environment.setBackgroundVideo('myplayer'); // or you can use player._player which is same value
+// Or, play in-line on a surface
+Environment.setScreen(
+  'default', /* screen name */
+  'myplayer', /* player unique id */
+  'default', /* surface name */
+  0, 0, 600, 400 /* relative position on the surface */
+);
 
 export default class Hackathon_CommunityChallenge_2019 extends React.Component {
   render() {
